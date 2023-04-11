2c7c0753edc9569a6c666da03ff23659bee5f8b5
simcity4006 <114875423+simcity4006@users.noreply.github.com>
2023-04-12 02:41:25 +0600

    Add files via upload

---
+++ images slider/main.js
@@ -0,0 +1,23 @@
+var photos = ['img/one.png', 'img/second.png', 'img/three.jpg'];
+var imgTag = document.querySelector('img');
+
+var count = 0;
+
+function next() {
+  count++;
+  if (count >= photos.length) {
+    count = 0;
+    imgTag.src = photos[count];
+  } else {
+    imgTag.src = photos[count];
+  }
+}
+function prev() {
+  count--;
+  if (count < 0) {
+    count = photos.length - 1;
+    imgTag.src = photos[count];
+  } else {
+    imgTag.src = photos[count];
+  }
+}
